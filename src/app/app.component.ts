c6b16715c6e089795a3095dee79136f50be1afdd
CarlosCasimiroF <102533234+CarlosCasimiroF@users.noreply.github.com>
2024-04-30 18:43:52 -0500

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SideBarComponent} from "./public/components/side-bar/side-bar.component";
+import {LoginComponent} from "./public/pages/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SideBarComponent],
+  imports: [RouterOutlet, SideBarComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/public/pages/login/login.component.ts
+++ src/app/public/pages/login/login.component.ts
@@ -1,12 +1,40 @@
+
+// src/app/login/login.component.ts
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
+  standalone:true,
+  imports: [MatCardModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    remember: false
+  });
+  hide = true;
 
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form data', this.loginForm.value);
+      // Implement your login logic here
+    }
+  }
 }
