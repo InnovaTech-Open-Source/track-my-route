4d54b3cf4960785606857a3f0780dfc6ac6162e1
CarlosCasimiroF <102533234+CarlosCasimiroF@users.noreply.github.com>
2024-04-30 22:37:58 -0500

    register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SideBarComponent} from "./public/components/side-bar/side-bar.component";
 import {LoginComponent} from "./public/pages/login/login.component";
+import {RegisterComponent} from "./public/pages/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SideBarComponent,LoginComponent],
+  imports: [RouterOutlet, SideBarComponent,LoginComponent,RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/public/pages/register/register.component.ts
+++ src/app/public/pages/register/register.component.ts
@@ -1,12 +1,39 @@
+
+// src/app/login/login.component.ts
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-register',
-  standalone: true,
-  imports: [],
+  standalone:true,
+  imports: [MatCardModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  RegisterForm: FormGroup = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    remember: false
+  });
+  hide = true;
 
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    if (this.RegisterForm.valid) {
+      console.log('Form data', this.RegisterForm.value);
+      // Implement your login logic here
+    }
+  }
 }
