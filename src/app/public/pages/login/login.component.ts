2f96a0fb182c8bea49f6a7b0371cbcef4fb97499
Lalo12342468 <88848197+Lalo12342468@users.noreply.github.com>
2024-05-01 14:46:01 -0500

    Update: enrutamiento

--- src/app/public/pages/login/login.component.ts
+++ src/app/public/pages/login/login.component.ts
@@ -1,12 +1,48 @@
+
+// src/app/login/login.component.ts
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
+  standalone:true,
+  imports: [MatCardModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm: FormGroup = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    remember: false
+  });
+  hide = true;
 
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form data', this.loginForm.value);
+      // Implement your login logic here
+    }
+  }
+
+  redirectToSidebar(){
+    this.router.navigateByUrl('/sidebar');
+  }
+  redirectToRegister() {
+    this.router.navigateByUrl('/register');
+  }
 }
--- src/app/public/pages/register/register.component.ts
+++ src/app/public/pages/register/register.component.ts
@@ -1,12 +1,43 @@
+
+// src/app/login/login.component.ts
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  standalone: true,
-  imports: [],
+  standalone:true,
+  imports: [MatCardModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  RegisterForm: FormGroup = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+    remember: false
+  });
+  hide = true;
 
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  onSubmit() {
+    if (this.RegisterForm.valid) {
+      console.log('Form data', this.RegisterForm.value);
+      // Implement your login logic here
+    }
+  }
+
+  redirectToLogin() {
+    this.router.navigateByUrl('/login'); // Redirige hacia la ruta '/login'
+  }
 }
